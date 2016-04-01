0949818461eebf008bd5e9cca2e66d87f3aad9c5
Gupta <GUPTAAN6@novartis.net>
2016-04-01 21:29:07 +0200

    winBatchfiles

---
+++ Bat files/download_files.bat
@@ -0,0 +1,7 @@
+@echo off
+start putty.exe -load mysession -v -ssh guptaan6@pd79di00 -pw Guptankit@123456 -m C:\Ankit_Jan2016\Git\repo\Bat files\download_D79.txt
+exit
+start putty.exe -load mysession -v -ssh guptaan6@pq79di00 -pw Guptankit@123456 -m C:\Ankit_Jan2016\Git\repo\Bat files\download_Q79.txt
+exit
+start putty.exe -load mysession -v -ssh guptaan6@pp79di00 -pw Guptankit@123456 -m C:\Ankit_Jan2016\Git\repo\Bat files\download_P79.txt
+exit
