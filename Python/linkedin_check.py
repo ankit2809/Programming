8f74cf6b921904bef93c32477427cac2c2fa69f7
ankit2809 <er.ankit.2809@gmail.com>
2024-01-03 23:38:17 +0100

    Update linkedin_check.py

--- Python/linkedin_check.py
+++ Python/linkedin_check.py
@@ -1,6 +1,6 @@
 from linkedin import linkedin
-API_KEY='77g9m61okx6drd'
-API_SECRET='bjcw05JLqyW0TJnM'
+API_KEY='xxxxxxx'
+API_SECRET='xxxxxxxxx'
 RETURN_URL='https://test.com'
 auth = linkedin.LinkedInAuthentication(API_KEY,API_SECRET,RETURN_URL)
 app = linkedin.LinkedInApplication(auth)
